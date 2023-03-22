2c8a3a330f610bc7c94bf008d124f92e179b22f1
leonperriman <leonperriman@gmail.com>
2023-03-22 20:18:52 +0100

    Iniciamos repositorio

---
+++ login.js
@@ -0,0 +1,61 @@
+function validarCredencialesFacil(correo, password) {
+
+    var listaUsuarios = [
+        { email: 'leontrollas.retbe', password: 'leonwasd' },
+        { email: 'ares45.retbe', password: 'llll'},
+        { email: 'chuchiperriman.retbe', password: 'leonyaresmejores'},
+        { email: 'tamarita.retbe' , password: 'tamarita123'},
+        { email: 'gomas.retbe' , password: 'XD'},
+        { email: 'usuario.admin', password: 'n1l1ayvzz'},
+    ];
+
+    for ( usuario in listaUsuarios) {
+        if (usuario.email === correo && usuario.password === password) {
+            return true;
+        }  
+    }
+
+    if (correo === 'leontrollas.retbe' && password === 'leonwasd') {
+        return true;
+    } else if (correo === 'ares45.retbe' && password === 'llll') {
+       return true;
+    }  else if (correo === 'chuchiperriman.retbe' && password === 'leonyaresmejores') {
+        return true;
+    } else if (correo === 'usuario.admin' && password === 'n1l1ayvzz') {
+        return true;
+    } else if (correo === 'tamarita.retbe' && password === 'tamarita123') {
+        return true;
+    } else if (correo === 'gomas.retbe' && password === 'XD') {
+        return true;
+    } else {
+       alert('Usuario y contraseña incorrectos');
+       return false;
+    }
+}
+
+function ingresarFacil(correo) {
+    if (correo === 'usuario.admin') {
+        window.location.href ='admin.html';
+    } else {
+        window.location.href ='escritorio.html';
+    }
+}
+
+function iniciarSesion(){
+    var sCorreo = '';
+    var sContrasenna = '';
+    var bAcceso = false;
+
+    sCorreo = document.querySelector('#txtCorreo').value;
+    sContrasenna = document.querySelector('#txtContrasenna').value;
+
+    bAcceso = validarCredencialesFacil(sCorreo, sContrasenna);
+    
+    if(bAcceso == true){
+        ingresarFacil(sCorreo);
+    }
+}
+
+
+var boton = document.getElementById('btnIngresar');
+boton.addEventListener('click', iniciarSesion);
