6c2174784a78200fc901b57a8c53052be951134c
leonperriman <leonperriman@gmail.com>
2023-04-20 17:20:43 +0200

    commander!!!

--- commander/comandos.js
+++ commander/comandos.js
@@ -2,13 +2,34 @@ var inputComando = document.getElementById("comando");
 var botonEjecutar = document.getElementById("botonEjecutar");
 
 // Borrar después de pruebas
-inputComando.value = '/ban chuchi';
+//inputComando.value = '/ban chuchi';
 
 botonEjecutar.addEventListener('click', function() {
    var comando = inputComando.value;
    var listaPartes = comando.split(' '); // [0,1,2,3,4,5] -> comando[0]
 
    if (listaPartes[0] === '/ban') {
-      alert("El usuario " + listaPartes[1] + " ha sido baneado.")
+      alert(listaPartes[1] + " ha sido baneado")
    }
+
+   if (listaPartes[0] === '/add') {
+      alert(listaPartes[1] + " ha sido añadido")
+   }
+
+   if (listaPartes[0] === '/lock') {
+      alert("El sistema ha sido bloqueado")
+   }
+
+      if (listaPartes[0] === '/unlock') {
+         alert("El sistema ha sido desbloqueado")
+      } 
+
+      if (listaPartes[0] === '/notes') {
+         alert("La nota de " + listaPartes[1] + " del " + listaPartes[2] + " es un " + listaPartes[3])
+      }
+//else {
+//      alert ('Comando no encontrado')
+//   }
 });
+
+   
